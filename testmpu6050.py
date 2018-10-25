b8529fb6c14e79df9018d8f4b06977b19a92b277
philipgoule <philip_goule@msn.com>
2018-10-26 01:24:44 +0800

    mpu6050

---
+++ testmpu6050.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+import os
+import time
+from mpu6050 import mpu6050
+sensor = mpu6050(0x68)
+while 1:
+	i=os.system('clear')
+	accel_data = sensor.get_accel_data()
+	print(accel_data['x'])
+	print(accel_data['y'])
+	print(accel_data['z'])
+	gyro_data = sensor.get_gyro_data()
+	print(gyro_data['x'])
+	print(gyro_data['y'])
+	print(gyro_data['z'])
+	time.sleep(0.3)
