37dc2503646de1c41dd24b6a9a6870b5e5d66df0
philipgoule <philip_goule@msn.com>
2023-05-18 08:36:08 +0800

    .zshrc

--- linuxdeploy.sh
+++ linuxdeploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
-cp ~/temp/.zshrc.pcbak ~/.zshrc
 cp ~/temp/.vimrc ~/.vimrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp ~/temp/.zshrc.pcbak ~/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
